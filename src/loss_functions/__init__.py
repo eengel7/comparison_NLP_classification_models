2bf04f145d3b52a02ff0f000b5de73e3c3856500
eengel7 <eengel7@icloud.com>
2023-03-20 09:38:19 +0100

    Adapt transformer code - working

---
+++ src/loss_functions/__init__.py
@@ -0,0 +1,3 @@
+from src.loss_functions.focal_loss import FocalLoss
+from src.loss_functions.dice_loss import DiceLoss
+from src.loss_functions.tversky_loss import TverskyLoss
--- src/run_training.py
+++
@@ -1,4 +0,0 @@
-
-def main(cfg):
-
-    print('Start training')
